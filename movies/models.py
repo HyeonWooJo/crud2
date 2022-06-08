4d88c27b4a713ba879926b9ee13c68314d449f72
HyeonWooJo <HyeonWooJo@github.com>
2022-06-08 14:56:10 +0900

    Update: models.py / makemigrations / migrate

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Actor2(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    date_of_birth = models.DateField()
+
+    class Meta:
+        db_table = "actors2"
+
+
+class Movie2(models.Model):
+    title = models.CharField(max_length=45)
+    release_date = models.DateField()
+    running_time = models.IntegerField()
+    actor = models.ManyToManyField("Actor2")
+
+    class Meta:
+        db_table = "movies2"
